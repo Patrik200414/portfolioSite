7357bf52a4b6880a9e4e0e27072d1955df52e6f3
Patrik200414 <martinpatrik14@gmail.com>
2023-02-13 06:43:35 +0100

    Add the nav ad nav-menu elements to the toggle function

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,7 @@
 const menueButton = document.querySelector('.menu-btn');
 const burger = document.querySelector('.menu-btn-burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
 
 let showMenu = false;
 
@@ -7,4 +9,6 @@ menueButton.addEventListener('click', toggleMenu);
 
 function toggleMenu(){
     burger.classList.toggle('open');
+    nav.classList.toggle('open');
+    menuNav.classList.toggle('open');
 }
