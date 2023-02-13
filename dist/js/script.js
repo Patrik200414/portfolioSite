2b1e5a2bd35cb896e8762099d5204813c90cf1b6
Patrik200414 <martinpatrik14@gmail.com>
2023-02-13 06:54:42 +0100

    Toggle the open class on the nav item elements with forEach

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,6 +2,7 @@ const menueButton = document.querySelector('.menu-btn');
 const burger = document.querySelector('.menu-btn-burger');
 const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav-item');
 
 let showMenu = false;
 
@@ -11,4 +12,5 @@ function toggleMenu(){
     burger.classList.toggle('open');
     nav.classList.toggle('open');
     menuNav.classList.toggle('open');
+    navItems.forEach(item => item.classList.toggle('open'));
 }
